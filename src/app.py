ef3a541bbbedf299ce28545ba1e2357cfc6e1f74
AnthonnyMal <74606871+AnthonnyMal@users.noreply.github.com>
2025-12-19 10:30:39 +0000

    Subida del modelo

--- src/app.py
+++ src/app.py
@@ -1,4 +1,14 @@
-from utils import db_connect
-engine = db_connect()
+##from utils import db_connect
+##engine = db_connect()
 
 # your code here
+
+from pickle import load
+import streamlit as st
+
+model = load(open("ranfor_classifier_nestimators-100_18.pkl", "rb"))
+
+st.write("""
+# My first app
+Hello *world!*
+""")
